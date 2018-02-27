e41e6bee7ecd3f961cfad3ff8c242146326fd58c
HideakiTouhara <hideee6723@gmail.com>
2018-02-27 15:17:13 +0900

    Observe TinderCardViewDataSource in sample code

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,19 +9,32 @@
 import UIKit
 import TinderCard
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, TinderCardViewDataSource {
+    
+    @IBOutlet weak var tinderCardView: TinderCardView!
+    
+    var sampleCards = [UIView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        for i in (0..<2) {
+            sampleCards.append(UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100)))
+            sampleCards[i].backgroundColor = UIColor.red
+        }
+        tinderCardView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfCards(_ tinderCard: TinderCardView) -> Int {
+        return 2
+    }
 
-
+    func tinderCard(_ tinderCard: TinderCardView, viewForCardAt index: Int) -> UIView {
+        return sampleCards[index]
+    }
 }
 
