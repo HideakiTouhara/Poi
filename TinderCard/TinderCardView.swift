b8f900cdf19464c4e9bef2ad902622751505e4a6
HideakiTouhara <hideee6723@gmail.com>
2018-02-27 15:16:35 +0900

    Add basicCard

--- TinderCard/TinderCardView.swift
+++ TinderCard/TinderCardView.swift
@@ -8,20 +8,22 @@
 
 import UIKit
 
-protocol TinderCardViewDataSource: class {
+public protocol TinderCardViewDataSource: class {
     func numberOfCards(_ tinderCard: TinderCardView) -> Int
     func tinderCard(_ tinderCard: TinderCardView, viewForCardAt index: Int) -> UIView
 }
 
-class TinderCardView: UIView {
+public class TinderCardView: UIView {
+    
+    var contentViews = [UIView]()
+    var basicView = UIView()
+    
     override public init(frame: CGRect) {
         super.init(frame: frame)
-        setUp()
     }
     
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        setUp()
     }
     
     public weak var dataSource: TinderCardViewDataSource? {
@@ -30,12 +32,28 @@ class TinderCardView: UIView {
         }
     }
     private func setUp() {
+        self.backgroundColor = UIColor.clear
+        let countOfCards = dataSource?.numberOfCards(self) ?? 0
+        for i in (0..<countOfCards) {
+            contentViews.append(createCard(index: i))
+        }
+        for i in (0..<countOfCards).reversed() {
+            self.addSubview(contentViews[i])
+        }
+        basicView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.width, height: self.frame.height))
+        self.addSubview(basicView)
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(_:)))
-        self.addGestureRecognizer(panGesture)
+        self.basicView.addGestureRecognizer(panGesture)
+    }
+    
+    private func createCard(index: Int) -> UIView {
+        if let dataSource = dataSource {
+            return dataSource.tinderCard(self, viewForCardAt: index)
+        }
+        return UIView()
     }
     
     @objc func panGesture(_ sender: UIPanGestureRecognizer) {
-        print("pan")
         let location = sender.translation(in: self)
         self.center = CGPoint(x: self.center.x + location.x, y: self.center.y + location.y)
     }
