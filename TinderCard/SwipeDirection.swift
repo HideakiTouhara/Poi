1c1b513266684290bf1922ff7df5a024a88360e5
HideakiTouhara <hideee6723@gmail.com>
2018-02-28 18:57:41 +0900

    Create SwipeDirection

---
+++ TinderCard/SwipeDirection.swift
@@ -0,0 +1,15 @@
+//
+//  SwipeDirection.swift
+//  TinderCard
+//
+//  Created by HideakiTouhara on 2018/02/28.
+//  Copyright © 2018年 HideakiTouhara. All rights reserved.
+//
+
+import Foundation
+
+public enum SwipeDirection: String {
+    case left
+    case right
+    case yetRunOut
+}
