8ec044226c40d0d76276e09bb93c5d0999172908
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2019-04-14 12:53:30 +0800

    Make the distance to send card away configurable

--- Poi/PoiView.swift
+++ Poi/PoiView.swift
@@ -27,6 +27,8 @@ public extension PoiViewDataSource {
 
 public class PoiView: UIView {
     
+    public var distanceToSendCardAway: CGFloat = 400
+    
     private var contentViews = [UIView]()
 
     private var currentCount = 0
@@ -149,7 +151,7 @@ public class PoiView: UIView {
         if sender.state == UIGestureRecognizer.State.ended {
             if overlay.center.x < 75 {
                 UIView.animate(withDuration: 0.4, animations: {
-                    card.center = CGPoint(x: card.center.x - 400, y: card.center.y)
+                    card.center = CGPoint(x: card.center.x - self.distanceToSendCardAway, y: card.center.y)
                 })
                 currentCount += 1
                 delegate?.poi(self, didSwipeCardAt: currentCount, in: .left)
@@ -158,7 +160,7 @@ public class PoiView: UIView {
                 }
             } else if overlay.center.x > (windowView.frame.width - 75) {
                 UIView.animate(withDuration: 0.4, animations: {
-                    card.center = CGPoint(x: card.center.x + 400, y: card.center.y)
+                    card.center = CGPoint(x: card.center.x + self.distanceToSendCardAway, y: card.center.y)
                 })
                 currentCount += 1
                 delegate?.poi(self, didSwipeCardAt: currentCount, in: .right)
